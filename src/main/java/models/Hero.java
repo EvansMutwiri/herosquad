3cd13a3b79290056a1fa1f39e5064fd4d260f050
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-13 16:33:23 +0300

    Logic and testing

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,49 @@
+package models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Hero {
+    private String content;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
+
+    public Hero(String content) {
+        this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Hero> getAll(){
+        return instances;
+    }
+
+    public static void clearAllHeros() {
+        instances.clear();
+    }
+
+    public boolean getPublished() {
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Hero findById(int id){
+        return instances.get(id-1);
+    }
+}
+
---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,85 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void NewHeroObjectGetsCorrectlyCreated_true() throws Exception {
+        Hero post = new Hero("Hero Nakamora");
+        assertEquals(true, post instanceof Hero);
+    }
+
+    @Test
+    void NewHeroObjectInstantiatesWithContent_true() {
+        Hero post = new Hero("Hero Nakamora");
+        assertEquals("Hero Nakamora", post.getContent());
+    }
+
+    @AfterEach
+    void tearDown() {
+        Hero.clearAllHeros();
+    }
+
+    @Test
+    public void AllHerosAreCorrectlyReturned_true() {
+        Hero post = new Hero("Hero Nakamora");
+        Hero otherHero = new Hero("Captain Ameruca");
+        assertTrue(Hero.getAll().contains(post));
+        assertTrue(Hero.getAll().contains(otherHero));
+    }
+
+    @Test
+    public void AllHerosContainsAllHeros_true() {
+        Hero post = new Hero("Hero Nakamora");
+        Hero otherHero = new Hero("Captain Ameruca");
+        assertTrue(Hero.getAll().contains(post));
+        assertTrue(Hero.getAll().contains(otherHero));
+    }
+
+    @Test
+    public void getPublished_isFalseAfterInstantiation_false() throws Exception {
+        Hero myHero = new Hero("Hero Nakamora");
+        assertEquals(false, myHero.getPublished());
+    }
+
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception{
+        Hero myHero = setupNewHero();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myHero.getCreatedAt().getDayOfWeek());
+    }
+
+    public Hero setupNewHero(){
+        return new Hero("Hero Nakamora");
+    }
+
+    @Test
+    public void getId_postsInstantiateWithAnID_1() throws Exception{
+        Hero.clearAllHeros();
+        Hero myHero = new Hero("Hero Nakamora");
+        assertEquals(1, myHero.getId());
+    }
+
+    @Test
+    public void findReturnsCorrectHero() throws Exception {
+        Hero post = setupNewHero();
+        assertEquals(1, Hero.findById(post.getId()).getId());
+    }
+
+    @Test
+    public void findReturnsCorrectHeroWhenMoreThanOneHeroExists() throws Exception {
+        Hero post = setupNewHero();
+        Hero otherHero = new Hero("Captain Ameruca");
+        assertEquals(2, Hero.findById(otherHero.getId()).getId());
+    }
+}
