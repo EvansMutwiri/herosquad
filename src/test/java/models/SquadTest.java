79add8fe4be418898eba97594f52d892f4e411b1
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-15 22:27:20 +0300

    create tests for squad

---
+++ src/test/java/models/SquadTest.java
@@ -0,0 +1,54 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCause() {
+    }
+
+    @Test
+    void getMaximum() {
+    }
+
+    @Test
+    void clearAllSquad() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void getHeroes() {
+    }
+
+    @Test
+    void addHero() {
+    }
+}
