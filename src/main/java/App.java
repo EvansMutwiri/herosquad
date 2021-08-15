e4100b4edf1d66c9e43f0155f7c69e44cc3411c3
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-16 02:15:01 +0300

    add images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        Integer port;
+        if (processBuilder.environment().get("PORT") != null) {
+            port= Integer.parseInt(processBuilder.environment().get("PORT"));
+        }else {
+            port =4567;
+        }
+        port(port);
         staticFileLocation("/public"); //app executable method
 
         post("/posts/new", (request, response) -> { //URL to make new post on POST route
@@ -43,6 +53,16 @@ public class App {
             return new ModelAndView(model,"squadform.hbs");
         },new HandlebarsTemplateEngine());
 
+        post("/squad/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            String cause = req.queryParams("cause");
+            int maximum = Integer.parseInt(req.queryParams("maximum"));
+            Squad squad = new Squad(name,cause,maximum);
+            model.put("squad", squad);
+            return new ModelAndView(model,"success-squad.hbs");
+        },new HandlebarsTemplateEngine());
+
         //Read
         get("/posts/:id", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
@@ -50,6 +70,13 @@ public class App {
             Hero foundHero = Hero.findById(idOfHeroToFind);
             model.put("post", foundHero); //add it to model for template to display
             return new ModelAndView(model, "herodetail.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/squad/list", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+            return new ModelAndView(model, "squad-details.hbs");
         }, new HandlebarsTemplateEngine());
 
         //Update get
